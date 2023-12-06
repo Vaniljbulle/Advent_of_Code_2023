a4718c592d8d267852f8d6fbd3ab71efa42f263c
vaniljbulle <vinxad@gmail.com>
2023-12-06 12:58:42 +0100

    Day 6

---
+++ Day 6/Puzzle 2/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <tuple>
+#include <climits>
+#include <valarray>
+#include <cmath>
+#include <algorithm>
+
+int main() {
+    std::vector<std::string> input;
+    std::string temp;
+    std::ifstream fIn("../../input.txt");
+    while (std::getline(fIn, temp)) {
+        if (temp.empty()) continue;
+        input.push_back(temp);
+    }
+    fIn.close();
+
+
+    std::stringstream ssTimes(input[0]); ssTimes >> temp;
+    std::stringstream ssDistances(input[1]); ssDistances >> temp;
+
+    std::vector<double> times, distances;
+    std::string value;
+    while (ssTimes >> temp) value += temp;
+    times.push_back(std::stod(value));
+    value = "";
+    while (ssDistances >> temp) value += temp;
+    distances.push_back(std::stod(value));
+
+    int product = 1;
+
+    // time = t, distance = d, time button held down = h
+    // d = h * (t - h)
+    // h^2 - ht + d = 0, solve for h
+
+    for (int i = 0; i < times.size(); ++i) {
+        double t = times[i];
+        double d = distances[i] + 1;
+        double dis = t * t - 4 * d;
+        double h1 = (t + std::sqrt(dis)) / 2;
+        double h2 = (t - std::sqrt(dis)) / 2;
+
+        int lower_bound = std::max(0, (int)(std::ceil(h2)));
+        int upper_bound = std::min(t, (std::floor(h1)));
+        product *= (upper_bound - lower_bound + 1);
+    }
+
+
+    std::cout << product << std::endl;
+    return 0;
+}
